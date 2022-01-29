32b4b99adbd3cb6e80e97a21a5e57218a1c7b491
Joseph Chantiny <jmchanti@gmail.com>
2022-01-29 09:45:47 -0600

    Initial commit

---
+++ payCalculator.py
@@ -0,0 +1,12 @@
+def calculatePay():
+    
+    # This first line is provided for you
+    hrs = input("Enter Hours:")
+    
+    # end assignment
+
+## if you want to test locally before you try to sync
+## uncomment calculatePay() and run > python payCalculator.py
+## ***IMPORTANT*** please recomment before you submit/sync your assignment.
+## OR YOUR TEST WILL NOT RUN
+# calculatePay()
