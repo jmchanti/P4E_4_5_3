1d3d956ccaad5ffd049f61836f38049e9d7c510a
Joseph Chantiny <jmchanti@gmail.com>
2022-02-01 09:35:00 -0600

    Update testProgram.py

--- testProgram.py
+++ testProgram.py
@@ -3,7 +3,7 @@ from random import randint
 import random
 from unittest.mock import Mock
 import compoundInterest
-
+​
 def test_monkeyCalculator_noSmiles_prints_correct_result(capfd, monkeypatch):
     principal_1 = 1200
     time_1 = 2
@@ -15,10 +15,9 @@ def test_monkeyCalculator_noSmiles_prints_correct_result(capfd, monkeypatch):
     time_3 = 1
     rate_3 = 1
     input = [principal_1, time_1, rate_1, principal_2, time_2, rate_2, principal_3, time_3, rate_3]
-    stingInput = str(principal_1)+"\n"+str(time_1)+"\n"+str(rate_1)+"\n"+ str(principal_2)+"\n"+ str(time_2)+"\n"+ str(rate_2)+"\n"+ str(principal_3)+"\n"+ str(time_3)+"\n"+ str(rate_3)+"\n"
-    monkeypatch.setattr('sys.stdin', stingInput)
+    monkeypatch.setattr('builtins.input', lambda _:input.pop(0))
     compoundInterest.calculateCompoundInterest()
-
+​
     out, err = capfd.readouterr()
     expected = "Compound Interest: 133.1\n---\nCompound Interest: 8394.89\n---\nCompound Interest: 0.5\n"
     assert out == expected
