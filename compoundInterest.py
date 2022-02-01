f1fe623b664337953f00472b107082035b6e50e3
Joseph Chantiny <jmchanti@gmail.com>
2022-02-01 10:06:11 -0600

    Update compoundInterest.py

--- compoundInterest.py
+++ compoundInterest.py
@@ -1,11 +1,10 @@
 def calculateCompoundInterest():
     
 # This first 3 lines are provided for yougetACompoundIntrest()
- getACompoundIntrest()
- print("---")
- getACompoundIntrest()
- print("---")
- getACompoundIntrest()
+# This first 3 lines are provided for you
+ client_one_principal = float(input("Principle (amount): "))
+ client_one_time =      float(input("Time:               "))
+ client_one_rate =      float(input("Rate:               "))
  #print("Compound Interest: "+str(intrest))
 
     # end assignment
@@ -14,13 +13,5 @@ def calculateCompoundInterest():
 ## uncomment calculateCompoundInterest() and run > python monkeyCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-def getACompoundIntrest():
- client_one_principal = float(input("Principle (amount): "))
- client_one_time =      float(input("Time:               "))
- client_one_rate =      float(input("Rate:               "))+100
- intrest = client_one_principal*pow((client_one_rate/100),client_one_time)
- CI = intrest-client_one_principal
- roundedIntrest = round(CI, 2)
- print("Compound Interest:",str(roundedIntrest))
 
 #calculateCompoundInterest()
